a2ee324c821f0cabcd8b7d3407c6bc13d34144d9
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-03-02 15:29:06 +0700

    FEAT: log out button

--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -1,11 +1,15 @@
 "use client";
+import { Button } from "@/components/ui/button";
+import logout from "@/server/actions/logout";
 import { useSession } from "next-auth/react";
 
 export default function Page() {
   const session = useSession();
+
   return (
     <div>
       <h1>{JSON.stringify(session)}</h1>
+      <Button onClick={async () => await logout()}>Log out</Button>
     </div>
   );
 }
