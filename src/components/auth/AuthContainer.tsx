51c71a2bb3d7b09f84ba87a5622210b0d317a2e8
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-03-03 15:23:07 +0700

    FEAT: add error variant

--- src/components/auth/AuthContainer.tsx
+++ src/components/auth/AuthContainer.tsx
@@ -1,11 +1,14 @@
+"use client";
+import { signIn } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
+import { FaGithub } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 import SeparatorWithText from "../separator/SeparatorWithText";
 import { Button } from "../ui/button";
 
 interface AuthContainerProps {
-  variant: "login" | "register";
+  variant: "login" | "register" | "error";
   children: React.ReactNode;
   headerLabel?: string;
   backButtonLabel?: string;
@@ -25,31 +28,60 @@ export default function AuthContainer({
   authLabel,
 }: AuthContainerProps) {
   return (
-    <div className="flex flex-col xl:w-72 justify-center items-center min-h-screen m-auto">
+    <div className="flex flex-col xl:w-[400px] justify-center items-center min-h-screen m-auto">
       <h3 className="text-2xl mb-6 font-extrabold">
         {headerLabel ?? variant === "login"
           ? "Welcome back"
+          : variant === "error"
+          ? "Oops! Something went wrong!"
           : "Create an account"}
       </h3>
       {children}
-      <SeparatorWithText label="Or" />
-      <Button disabled={loading} className="w-full">
-        <FcGoogle className="mr-2" />
-        {loading ? "Loading..." : `${authLabel ?? variant} with Google`}
-      </Button>
+      {variant !== "error" && (
+        <>
+          <SeparatorWithText label="Or" />
+          <section className="flex flex-col gap-2 w-full">
+            <Button
+              disabled={loading}
+              className="w-full"
+              onClick={() => {
+                signIn("google");
+              }}>
+              <FcGoogle className="mr-2" />
+              {loading ? "Loading..." : `${authLabel ?? variant} with Google`}
+            </Button>
+            <Button
+              disabled={loading}
+              className="w-full"
+              onClick={() => {
+                signIn("github");
+              }}>
+              <FaGithub className="mr-2" />
+              {loading ? "Loading..." : `${authLabel ?? variant} with Github`}
+            </Button>
+          </section>
+        </>
+      )}
 
       <section className="flex w-full justify-center items-center">
-        <p>
-          {backButtonLabel ?? variant === "login"
-            ? "Don't have an account?"
-            : "Already have an account?"}
-        </p>
+        {variant !== "error" && (
+          <p>
+            {backButtonLabel ?? variant === "login"
+              ? "Don't have an account?"
+              : "Already have an account?"}
+          </p>
+        )}
         <Button variant="link">
           <Link
             href={
-              hrefBack ?? `${variant === "login" ? "/register" : "/login"}`
+              hrefBack ??
+              `${variant === "login" ? "/auth/register" : "/auth/login"}`
             }>
-            {hrefBackLabel ?? variant === "login" ? "Register" : "Login"}
+            {hrefBackLabel ?? variant === "login"
+              ? "Register"
+              : variant === "error"
+              ? "Back to login"
+              : "Login"}
           </Link>
         </Button>
       </section>
