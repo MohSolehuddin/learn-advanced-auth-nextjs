b50acc07681ffb6a2911017f72545351367954af
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-03-03 15:27:57 +0700

    FEAT: auto verify token if login with OAuth provider

--- src/auth.ts
+++ src/auth.ts
@@ -9,6 +9,18 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   session: {
     strategy: "jwt",
   },
+  events: {
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: { id: user.id },
+        data: { emailVerified: new Date() },
+      });
+    },
+  },
+  pages: {
+    signIn: "/auth/login",
+    error: "/auth/error",
+  },
   callbacks: {
     async session({ token, session }) {
       if (token.sub && session.user) {
