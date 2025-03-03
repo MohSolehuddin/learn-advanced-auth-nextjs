ccc6ca691485b780437775fa05b6a38674dc780b
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-03-03 15:25:52 +0700

    FEAT: add get user by id

--- src/data/user.ts
+++ src/data/user.ts
@@ -3,3 +3,6 @@ import { db } from "@/server/db";
 export async function getUserByEmail(email: string) {
   return db.user.findUnique({ where: { email } });
 }
+export async function getUserById(id: string) {
+  return db.user.findUnique({ where: { id } });
+}
