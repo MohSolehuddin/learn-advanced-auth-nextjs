1e33d13c148fd08a6ef3ff4bc33d9cb7951ce78c
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-03-04 13:43:17 +0700

    feat: get and create verify token

--- src/data/verificationToken.ts
+++ src/data/verificationToken.ts
@@ -26,11 +26,66 @@ export const getVerificationTokenByEmail = async (email: string) => {
   }
 };
 
+export const getVerificationTokenByToken = async (token: string) => {
+  try {
+    return db.verificationToken.findFirst({
+      where: {
+        token,
+        deletedAt: null,
+      },
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getAllVerificationToken = async () => {
+  try {
+    return db.verificationToken.findMany({
+      take: 5,
+      orderBy: {
+        expires: "desc",
+      },
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deleteVerificationTokenByID = async (id: string) => {
+  try {
+    return db.verificationToken.delete({
+      where: {
+        id,
+      },
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
 export const deleteExistingVerificationTokenByEmail = async (email: string) => {
   try {
     return db.verificationToken.deleteMany({
       where: {
         email,
+      },
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const softDeleteExistingVerificationTokenByEmail = async (
+  email: string
+) => {
+  try {
+    return db.verificationToken.updateMany({
+      where: {
+        email,
+      },
+      data: {
+        deletedAt: new Date(),
       },
     });
   } catch (error) {
