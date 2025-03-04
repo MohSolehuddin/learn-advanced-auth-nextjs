c0f68df2b7773321a05c0ffa640f083a065b427c
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-03-04 13:50:47 +0700

    fix: login action

--- src/server/actions/login.ts
+++ src/server/actions/login.ts
@@ -1,11 +1,8 @@
 "use server";
 
 import { signIn } from "@/auth";
-import { getUserByEmail, makeEmailIsVerified } from "@/data/user";
-import { getVerificationTokenByEmail } from "@/data/verificationToken";
+import { getUserByEmail } from "@/data/user";
 import { LoginSchema } from "@/lib/schema/loginSchema";
-import senEmailVerification from "@/lib/sendEmailVerification";
-import { generateVerificationToken } from "@/lib/token";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { AuthError } from "next-auth";
 import { z } from "zod";
@@ -13,30 +10,15 @@ import { z } from "zod";
 export default async function login(values: z.infer<typeof LoginSchema>) {
   const validated = LoginSchema.safeParse(values);
   if (!validated.success) return { error: validated.error.message };
-  const { email, password, verificationToken } = validated.data;
+  const { email, password } = validated.data;
 
   const existingUser = await getUserByEmail(email);
   if (!existingUser) return { error: "Email or password is incorrect" };
 
-  const existingVerificationToken = await getVerificationTokenByEmail(email);
-
-  if (
-    !existingUser.emailVerified ||
-    verificationToken !== existingVerificationToken?.token
-  ) {
-    const verification = await generateVerificationToken(email);
-
-    if (verification?.status === "error")
-      return { error: verification.message };
-    const linkVerification = `http://localhost:3000/auth/verify-email?token=${verification?.data?.token}`;
-    await senEmailVerification(email, linkVerification, existingUser.name);
+  if (!existingUser.emailVerified) {
     return {
-      error:
-        "Email is not verified, new link verification email is resend, please check your email",
+      error: "Email is not verified, please check your email",
     };
-  }
-  if (verificationToken) {
-    await makeEmailIsVerified(email);
   }
 
   try {
@@ -46,7 +28,6 @@ export default async function login(values: z.infer<typeof LoginSchema>) {
       redirectTo: DEFAULT_LOGIN_REDIRECT,
     });
   } catch (error) {
-    console.log("Something", error);
     if (error instanceof AuthError) {
       switch (error.type) {
         case "CredentialsSignin":
