66dbbc5b01399ad625bf8240ec781d40fcad899f
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-03-06 16:39:26 +0700

    FIX: format otp url for google

--- src/server/actions/2fa/generate.ts
+++ src/server/actions/2fa/generate.ts
@@ -1,16 +1,13 @@
 "use server";
+import { auth } from "@/auth";
 import { authenticator } from "otplib";
 
 export async function generate2fa() {
+  const session = await auth();
   const secret = authenticator.generateSecret();
   const token = authenticator.generate(secret);
 
-  // Generate a QR Code URL for the authenticator app
-  const otpauthUrl = authenticator.keyuri(
-    "user@example.com",
-    "YourApp",
-    secret
-  );
+  const otpauthUrl = `otpauth://totp/learn-advanced-auth:${session?.user.email}?secret=${secret}&issuer=learn-advanced-auth`;
 
   return { secret, token, otpauthUrl };
 }
