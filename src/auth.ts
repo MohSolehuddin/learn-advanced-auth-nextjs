96192e15803df5bcd0c2202fb6fe09d86cc2ceac
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-03-06 13:24:00 +0700

    FEAT: custom JWT expiration and optimalize calling refresh session in database, only when signin and signup

--- src/auth.ts
+++ src/auth.ts
@@ -4,11 +4,13 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { UserRole } from "@prisma/client";
 import NextAuth from "next-auth";
 import { getUserById, makeEmailIsVerified } from "./data/user";
+import { JWT_EXPIRATION_IN_HOURS } from "./lib/constants";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(db),
   session: {
     strategy: "jwt",
+    maxAge: JWT_EXPIRATION_IN_HOURS,
   },
   events: {
     async linkAccount({ user }) {
@@ -31,23 +33,20 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       return session;
     },
     async jwt({ token, trigger, session }) {
-      console.log({ trigger });
       if (!token.sub) return token;
-
-      const existingUser = await getUserById(token.sub);
-      if (!existingUser) return token;
-
-      token.role = existingUser.role;
-      token.name = existingUser.name;
-      token.email = existingUser.email;
-      token.image = existingUser.image;
-
-      if (trigger === "update" && session) {
-        token.name = session.user.name;
-        token.email = session.user.email;
-        token.image = session.user.image;
+      if (trigger && trigger !== "update") {
+        const existingUser = await getUserById(token.sub);
+        if (!existingUser) return token;
+        token.role = existingUser.role;
+        token.name = existingUser.name;
+        token.email = existingUser.email;
+        token.image = existingUser.image;
       }
-
+      if (trigger === "update" && session) {
+        token.name = session.name;
+        token.email = session.email;
+        token.image = session.image;
+      }
       return token;
     },
   },
