dee2a8a017b0a308afcfc31d9114d87f6bdaa093
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-03-07 17:42:17 +0700

    FEAT: add OTP_verifed in user

--- src/next-auth.d.ts
+++ src/next-auth.d.ts
@@ -3,6 +3,7 @@ import { DefaultSession } from "next-auth";
 
 export type ExtendedUser = DefaultSession["user"] & {
   role: UserRole;
+  OTP_verified: boolean;
 };
 declare module "next-auth" {
   interface Session extends DefaultSession {
