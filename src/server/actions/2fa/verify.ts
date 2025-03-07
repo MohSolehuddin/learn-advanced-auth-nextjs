0f8be1b559bbe97bafc3a64e5ac09d8f9133a398
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-03-07 10:46:39 +0700

    FIX: make secret is optional and fix error response

--- src/server/actions/2fa/verify.ts
+++ src/server/actions/2fa/verify.ts
@@ -1,8 +1,29 @@
 "use server";
+import { auth } from "@/auth";
+import { getUserByEmail } from "@/data/user";
 import { authenticator } from "otplib";
 
-export async function verify2fa(secret: string, token: string) {
-  const isValid = authenticator.check(token, secret);
+export async function verify2fa({
+  token,
+  secret,
+}: {
+  token: string;
+  secret?: string;
+}) {
+  const session = await auth();
+  let existingUser;
+  if (!secret) existingUser = await getUserByEmail(session?.user?.email ?? "");
+
+  if (!existingUser?.twoFA_key && !secret)
+    return {
+      message: "Please enable Two Factor Authentication",
+      success: false,
+    };
+
+  const isValid = authenticator.check(
+    token,
+    secret ?? (existingUser?.twoFA_key as string)
+  );
 
   if (isValid) {
     return { message: "Token is valid", success: true };
