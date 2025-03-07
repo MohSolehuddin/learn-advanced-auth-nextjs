9b5be3cc23dbeb58e4611a48d0effe9105e1da81
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-03-07 17:43:37 +0700

    FEAT: add console debuged

--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -9,6 +9,7 @@ import { useEffect } from "react";
 
 export default function Page() {
   const { data: session, status, update } = useSession();
+  console.log({ session });
 
   const router = useRouter();
   useEffect(() => {
@@ -41,6 +42,9 @@ export default function Page() {
       <section>
         <h1>Hallo, {session?.user?.name}</h1>
         <h2>Your email, {session?.user?.email}</h2>
+        <h2>
+          IS OTP , {session?.user?.OTP_verified ? "VERIFIED" : "NOT VERIFIED"}
+        </h2>
       </section>
       <section className="flex gap-6">
         <Button onClick={onUpdateProfile}>Update Profile</Button>
